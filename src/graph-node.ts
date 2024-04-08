7a0c21d344f8fa5e342a414a3a018c4969dffcfa
Daniel Imal <mrdanimal@gmail.com>
2024-04-08 15:18:51 -0400

    remove cruft

--- src/graph-node.ts
+++ src/graph-node.ts
@@ -1,33 +1,23 @@
 import { IAttrHandler, createComponent } from './component.fp';
 
-const style = `
+const css = `
 :host {
   display: block;
   border: 2px solid black;
   padding: 10px;
   font-family: sans-serif;
 }
-
-:host(:hover) ::slotted(div) {
-  color: red;
-}
 `;
 
-const dom = `
+const template = `
   <div>
     <slot name="name">Node</slot>
   </div>
 `;
 
-const handleName: IAttrHandler = (self: HTMLElement, shadowRoot: ShadowRoot) => {
-  shadowRoot.querySelector('slot[name="title"]').textContent = self.getAttribute('name');
-};
-
 createComponent({
   componentName: 'graph-node',
-  template: dom,
-  css: style,
-  attrHandlers: {
-    title: handleName,
-  },
+  template,
+  css,
+  attrHandlers: {},
 });
