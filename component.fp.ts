921bb4ca0bc84cb93d233d5a2281241b4a93850e
Daniel Imal <mrdanimal@gmail.com>
2024-04-08 12:46:51 -0400

    tighten up

--- component.fp.ts
+++ component.fp.ts
@@ -1,5 +1,40 @@
-class MyComponent extends HTMLElement {}
+function createComponent(template: string, css: string, attributes: string[]) {
+  class Component extends HTMLElement {
+    #shadowRoot;
 
-function createComponent() {
-  return new MyComponent();
+    constructor() {
+      super();
+
+      this.#shadowRoot = this.attachShadow({
+        mode: "closed",
+        delegatesFocus: true,
+      });
+
+      this.#shadowRoot.innerHTML = `<style>${css}</style>${template}`;
+    }
+
+    connectedCallback() {
+      console.log("Custom element added to page.");
+      this.update();
+    }
+
+    static get observedAttributes() {
+      return attributes;
+    }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+      console.log(`Attribute ${attrName} changed from ${oldVal} to ${newVal}`);
+      this.update();
+    }
+
+    update() {
+      // Update component in response to attribute changes
+      if (this.hasAttribute("name")) {
+        this.#shadowRoot.querySelector('slot[name="title"]').textContent =
+          this.getAttribute("name");
+      }
+    }
+  }
+
+  return Component;
 }
