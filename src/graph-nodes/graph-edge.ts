cef9e825e4015abe110147f5a9ac8e5270f72572
Daniel Imal <mrdanimal@gmail.com>
2024-04-09 14:52:40 -0400

    extract path

--- src/graph-nodes/graph-edge.ts
+++ src/graph-nodes/graph-edge.ts
@@ -10,9 +10,15 @@ const css = `
 }
 `;
 
+const path = `
+  M0 10 C 20 20, 
+  40 20, 
+  50 10
+`;
+
 const template = `
   <svg width="50" height="20">
-    <path d="M0 10 C 20 20, 40 20, 50 10" stroke="black" fill="transparent"/>
+    <path d="${path}" stroke="black" fill="transparent"/>
   </svg>
 `;
 
