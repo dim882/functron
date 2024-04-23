84eee1abc8a98cde7287826edb7f9775e2fee7f7
Daniel Imal <mrdanimal@gmail.com>
2024-04-22 20:31:12 -0400

    formatting

--- src/my-component/my-component.ts
+++ src/my-component/my-component.ts
@@ -1,28 +1,26 @@
 import { IAttrHandler, ITemplateParams, createComponent } from '../component.fp.js';
 
 const css = `
-:host {
-  display: block;
-  border: 2px solid black;
-  padding: 10px;
-  font-family: sans-serif;
-}
+  :host {
+    display: block;
+    border: 2px solid black;
+    padding: 10px;
+    font-family: sans-serif;
+  }
 
-:host(:hover) ::slotted(div) {
-  color: red;
-}
+  :host(:hover) ::slotted(div) {
+    color: red;
+  }
 `;
 
 const template = (values: ITemplateParams) => {
-  console.log({ values });
-
   return `
-  <div>
-    <slot name="title">Default Title</slot>
-    <div><input type="text" name="first_name" value="${values.firstname}"></div>
-    <p><slot name="children"></slot></p>
-  </div>
-`;
+    <div>
+      <slot name="title">Default Title</slot>
+      <div><input type="text" name="first_name" value="${values.firstname}"></div>
+      <p><slot name="children"></slot></p>
+    </div>
+  `;
 };
 
 const handleTitle: IAttrHandler = ({ value }) => (value ? value : '');
