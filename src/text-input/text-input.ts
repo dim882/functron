859594325a5e8144661e9076c06ff53c7ead4dac
Daniel Imal <mrdanimal@gmail.com>
2024-04-23 20:40:09 -0400

    clean up example of closed DOM issue. clean up text-input

--- src/text-input/text-input.ts
+++ src/text-input/text-input.ts
@@ -1,38 +1,21 @@
 import { IAttrHandler, ITemplateParams, createComponent } from '../component.fp.js';
 
-const css = `
-  :host {
-    display: block;
-    border: 2px solid black;
-    padding: 10px;
-    font-family: serif;
-  }
-
-  :host(:hover) ::slotted(div) {
-    color: red;
-  }
-`;
-
-const template = (values: ITemplateParams) => {
-  return `
-    <div>
-      <label><slot name="label"></slot></label>
-      <div><input type="text" name="${values.fieldname}" value="${values.value}"></div>
-      <p class='my-message'><slot name="message"></slot></p>
-    </div>
-  `;
-};
-
 const defaultHandler: IAttrHandler = ({ value }) => (value ? value : '');
 
 const MyComponent = createComponent({
-  template,
-  // css,
   cssPath: './text-input.css',
-  shadowDomSettings: { mode: 'open' },
   attrHandlers: {
     fieldname: defaultHandler,
     value: defaultHandler,
+  },
+  template: (values: ITemplateParams) => {
+    return `
+      <div>
+        <label><slot name="label"></slot></label>
+        <div><input type="text" name="${values.fieldname}" value="${values.value}"></div>
+        <p class='my-message'><slot name="message"></slot></p>
+      </div>
+    `;
   },
 });
 
