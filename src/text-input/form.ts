336e6779697b60dfe3782dcd01b6480c845a167e
Daniel Imal <mrdanimal@gmail.com>
2024-04-23 20:18:04 -0400

    cleanup form

--- src/text-input/form.ts
+++ src/text-input/form.ts
@@ -1,21 +1,13 @@
 import { IAttrHandler, ITemplateParams, createComponent } from '../component.fp.js';
 
-const template = (values: ITemplateParams) => {
-  return `
-    <form>
-      <slot></slot>
-    </form>
-  `;
-};
-
-const defaultHandler: IAttrHandler = ({ value }) => (value ? value : '');
-
 const MyComponent = createComponent({
-  template,
   cssPath: './form.css',
-  attrHandlers: {
-    fieldname: defaultHandler,
-    value: defaultHandler,
+  template: (values: ITemplateParams) => {
+    return `
+      <form>
+        <slot></slot>
+      </form>
+    `;
   },
 });
 
