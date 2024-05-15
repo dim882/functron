9378ea1680ed07d4c6ec19ce5b3dd7e51b116311
Daniel Imal <mrdanimal@gmail.com>
2024-05-15 17:45:04 -0400

    organize imports

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -1,8 +1,7 @@
+import { h, render } from 'preact';
+
 import '../components/text-input/text-input';
 import '../components/text-input/form';
-
-// src/demo/demo.tsx
-import { h, render } from 'preact';
 
 const App = () => <h1>Hello World</h1>;
 
