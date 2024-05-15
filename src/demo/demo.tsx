b8a9bbccff04f23b9a8dd001b7e5461c1b834b0f
Daniel Imal <mrdanimal@gmail.com>
2024-05-15 17:44:24 -0400

    set up tsconfig for node imports. stub out demo component

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -1,2 +1,9 @@
 import '../components/text-input/text-input';
 import '../components/text-input/form';
+
+// src/demo/demo.tsx
+import { h, render } from 'preact';
+
+const App = () => <h1>Hello World</h1>;
+
+render(<App />, document.getElementById('app'));
