d1c4b150c318ef120ea5b453c551da65a77ab948
Daniel Imal <mrdanimal@gmail.com>
2024-05-15 18:41:28 -0400

    remove delete plugin that was breaking live reload

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
-import del from 'rollup-plugin-delete';
 import { babel } from '@rollup/plugin-babel';
 
 export default {
@@ -14,7 +13,6 @@ export default {
     sourcemap: true,
   },
   plugins: [
-    del({ targets: 'dist' }),
     nodeResolve(),
     typescript({
       tsconfig: './tsconfig.json',
