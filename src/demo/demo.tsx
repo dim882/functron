2264c2a47fbbb5cb5e1ce68a2b07a2b128ea2816
Daniel Imal <mrdanimal@gmail.com>
2024-05-21 14:43:08 -0400

    remove color picker

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -1,16 +1,3 @@
-// import { h, render } from 'preact';
-// import ColorPicker from '../components/color-picker/ColorPicker';
-
 import '../components/text-input/text-input';
 import '../components/text-input/form';
-
 import '../components/hello-vdom/hello-vdom';
-
-const App = () => {
-  const handleColorChange = (color: string) => {
-    // console.log({ color });
-  };
-  // return <ColorPicker onChange={handleColorChange} lch={[60, 140, 10]} />;
-};
-
-// render(<App />, document.getElementById('app'));
