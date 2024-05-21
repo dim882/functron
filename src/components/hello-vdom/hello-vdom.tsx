aff410c73686344cfe57fc48103ac52a83acbdd4
Daniel Imal <mrdanimal@gmail.com>
2024-05-21 14:49:58 -0400

    prove that a plain hello world works

--- src/components/hello-vdom/hello-vdom.tsx
+++ src/components/hello-vdom/hello-vdom.tsx
@@ -1,9 +1,27 @@
+import { jsx, Fragment, VNode } from 'snabbdom';
 import { createDecoratedComponent } from '../../lib/createComponent.decorated';
-import { h, jsx } from '../../lib/snabbdomHelper';
+import { h } from '../../lib/snabbdomHelper';
+
+const vNode: VNode = (
+  <form>
+    <ui-text-input attrs={{ fieldname: 'firstname', value: 'Joseph Schmoe' }}>
+      <div slot="label">Custom Label</div>
+      <div slot="message">Do something!</div>
+    </ui-text-input>
+    foo
+    <ui-text-input attrs={{ fieldname: 'firstname', value: 'Joseph Schmoe' }}></ui-text-input>
+  </form>
+);
+console.log('Transformed VNode:', vNode);
 
 const SnabbdomComponent = createDecoratedComponent({
   css: '.myClass { color: red; }',
-  render: (model) => <div class={{ myClass: true }}>This is JSX</div>,
+  render: (model) => (
+    <div class={{ myClass: true }}>
+      This is JSX <div>foo</div>
+      <span>bar</span>
+    </div>
+  ),
 });
 
 customElements.define('hello-vdom', SnabbdomComponent);
--- src/components/text-input/form.tsx
+++ src/components/text-input/form.tsx
@@ -2,24 +2,14 @@ import { jsx, Fragment, VNode } from 'snabbdom';
 import { createDecoratedComponent } from '../../lib/createComponent.decorated';
 import { h } from '../../lib/snabbdomHelper';
 
-const vNode: VNode = (
-  <form>
-    <div>
-      foo!<div>baz</div>
-    </div>
-    bar
-  </form>
-);
-console.log('Transformed VNode:', vNode);
-
 const MyComponent = createDecoratedComponent({
   cssPath: './form.css',
   render: (state) => (
     <form>
-      <div>
-        foo!<div>baz</div>
-      </div>
-      bar
+      <ui-text-input attrs={{ fieldname: 'firstname', value: 'Joseph Schmoe' }}>
+        <div slot="label">Custom Label</div>
+        <div slot="message">Do something!</div>
+      </ui-text-input>
     </form>
   ),
 });
