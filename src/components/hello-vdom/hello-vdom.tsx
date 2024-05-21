5c142aa6d21f816b66e65be82310fd5145d3c6e0
Daniel Imal <mrdanimal@gmail.com>
2024-05-21 15:00:20 -0400

    try to render an attribute

--- src/components/hello-vdom/hello-vdom.tsx
+++ src/components/hello-vdom/hello-vdom.tsx
@@ -2,25 +2,19 @@ import { jsx, Fragment, VNode } from 'snabbdom';
 import { createDecoratedComponent } from '../../lib/createComponent.decorated';
 import { h } from '../../lib/snabbdomHelper';
 
-const vNode: VNode = (
-  <form>
-    <ui-text-input attrs={{ fieldname: 'firstname', value: 'Joseph Schmoe' }}>
-      <div slot="label">Custom Label</div>
-      <div slot="message">Do something!</div>
-    </ui-text-input>
-    foo
-    <ui-text-input attrs={{ fieldname: 'firstname', value: 'Joseph Schmoe' }}></ui-text-input>
-  </form>
-);
-
 const SnabbdomComponent = createDecoratedComponent({
   css: '.myClass { color: red; }',
-  render: (model) => (
-    <div class={{ myClass: true }}>
-      This is JSX <div>foo</div>
-      <span>bar</span>
-    </div>
-  ),
+  attributes: ['foo'],
+  render: (model) => {
+    console.log({ model });
+
+    return (
+      <div class={{ myClass: true }}>
+        This is JSX
+        <div>{model?.foo}</div>
+      </div>
+    );
+  },
 });
 
 customElements.define('hello-vdom', SnabbdomComponent);
