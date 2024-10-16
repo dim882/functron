3c40cd963c1a1d35f4d0047083dfedaac12c1e49
Daniel Imal <mrdanimal@gmail.com>
2024-10-15 21:23:43 -0400

    extract funcs as consts

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -2,26 +2,30 @@ import { createComponent, EventHandler, jsx } from '../../lib/ComponentFactory';
 
 interface ICounterModel {
   count: number;
-};
+}
 
-const incrementCounter: EventHandler<ICounterModel, MouseEvent> = (event, model) => {
-  return ({
-    ...model,
-    count: model.count + 1
-  })
+const incrementCounter: EventHandler<ICounterModel, MouseEvent> = (event, model) => ({
+  ...model,
+  count: model.count + 1,
+});
+
+const initialModel: ICounterModel = { count: 0 };
+
+const render = ({ count }: ICounterModel, { onClick }: { onClick: EventHandler<ICounterModel, MouseEvent> }) => (
+  <div>
+    <button on={{ click: onClick }}>Add 1</button>
+    <div>{count}</div>
+  </div>
+);
+
+const handlers = {
+  onClick: incrementCounter,
 };
 
 const MyComponent = createComponent<[], ICounterModel>({
-  initialModel: { count: 0 },
-  handlers: {
-    onClick: incrementCounter
-  },
-  render: ({ count }, { onClick }) => (
-    <div>
-       <button on={{ click: onClick }}>Add 1</button>
-       <div>{count}</div> 
-    </div>
-  ),
+  initialModel,
+  handlers,
+  render,
 });
 
 customElements.define('ui-counter', MyComponent);
