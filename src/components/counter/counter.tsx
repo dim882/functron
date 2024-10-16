67db5a2910a43e4fa7b07194843bdeb81f96b9f1
Daniel Imal <mrdanimal@gmail.com>
2024-10-16 15:06:49 -0400

    make RenderFunc accept handler map

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -5,8 +5,8 @@ interface ICounterModel {
   count: number;
 }
 
-interface RenderFunc<TModel> {
-  (model: TModel, handlers: EventHandlerMap<TModel>): VNode;
+interface RenderFunc<TModel, THandlers extends Partial<EventHandlerMap<TModel>> = EventHandlerMap<TModel>> {
+  (model: TModel, handlers: THandlers): VNode;
 }
 
 const initialModel: ICounterModel = { count: 0 };
@@ -20,7 +20,10 @@ type EventHandlerMap<TModel> = {
   [key: string]: EventHandler<TModel, any>;
 };
 
-const render: RenderFunc<ICounterModel> = ({ count }, { incrementCounter }) => (
+const render: RenderFunc<ICounterModel, { incrementCounter: EventHandler<ICounterModel, MouseEvent> }> = (
+  { count },
+  { incrementCounter }
+) => (
   <div>
     <button on={{ click: incrementCounter }}>Add 1</button>
     <div>{count}</div>
