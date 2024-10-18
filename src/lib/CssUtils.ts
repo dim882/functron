b5a203e15b683765e92ebafd8d1628aeee6aa31c
Daniel Imal <mrdanimal@gmail.com>
2024-10-18 17:31:50 -0400

    fix css TS error

--- src/lib/CssUtils.ts
+++ src/lib/CssUtils.ts
@@ -1,15 +1,23 @@
-export async function applyCss(dom: ShadowRoot, cssPath: string | undefined, css: string) {
+export async function applyCss(dom: ShadowRoot, cssPath: string | undefined, css: string | undefined) {
   const style = document.createElement('style');
   dom.appendChild(style);
-  style.textContent = cssPath ? await loadCss(cssPath) : css;
+
+  if (cssPath) {
+    const loadedCss = await loadCss(cssPath);
+    style.textContent = loadedCss ?? '';
+  } else {
+    style.textContent = css ?? '';
+  }
 }
 
 async function loadCss(cssFilePath: string) {
   try {
     const response = await fetch(cssFilePath);
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
+
     return await response.text();
   } catch (error) {
     console.error('Failed to fetch CSS:', error);
